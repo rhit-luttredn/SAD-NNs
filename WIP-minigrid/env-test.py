030a2a57796edf1044e551fcc9d77168c8f2bb3a
rhit-luttredn <luttredn@rose-hulman.edu>
2024-01-31 23:24:39 -0500

    env test working

--- WIP-minigrid/env-test.py
+++ WIP-minigrid/env-test.py
@@ -1,42 +1,48 @@
 #! /usr/bin/env python3
 from dataclasses import dataclass
 
+from typing import Tuple, Union
 import sad_nns.envs
 import tyro
 from gymnasium import make
-
+from minigrid.manual_control import ManualControl
 
 @dataclass
 class Args:
-    env_id: str = "HardWall-v0"
+    env_id: str = "HardWallEnv-v0"
     """the environment id"""
-    render: str = "human"
+    render_mode: str = "human"
     """the rendering mode"""
-    env_size: int|None = 10
+    size: int|None = 10
     """the height and width of the environment"""
-    env_width: int|None = None
+    width: int|None = None
     """the width of the environment"""
-    env_height: int|None = None
+    height: int|None = None
     """the height of the environment"""
-    agent_start_pos: tuple|None = None,
+    agent_start_pos: tuple|None = None
     """the starting position of the agent"""
-    agent_start_dir: int = 0,
+    agent_start_dir: int = 0
     """the starting direction of the agent"""
-    env_max_steps: int|None = None,
+    max_steps: int|None = None
     """the maximum number of steps before the environment is terminated"""
-    see_through_walls: bool = True,
+    see_through_walls: bool = True
     """whether the agent can see through walls"""
-    wall_freq: int = 2,
+    wall_freq: int = 2
     """FOR HARDWALL: the number of tiles between walls"""
-    use_lava: bool = False,
+    use_lava: bool = False
     """FOR HARDWALL: whether to use lava"""
-    lock_doors: bool = False,
+    lock_doors: bool = False
     """FOR HARDWALL: whether to lock doors"""
 
 
 if __name__ == "__main__":
     args = tyro.cli(Args)
     args = vars(args)
+
     env_id = args.pop("env_id")
-    render = args.pop("render")
-    env = make(env_id, render=render, **args)
+    args = {k: v for k, v in args.items() if v is not None}
+    env = make(env_id, **args)
+    if args["render_mode"] == "human":
+        manual_control = ManualControl(env, seed=42)
+        manual_control.start()
+        env.render()
