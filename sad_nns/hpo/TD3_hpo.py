d88950d81cdecbc3edd5d260dbda67d5f99395a1
rhit-fioritjx <fioritjx@rose-hulman.edu>
2024-03-11 09:49:01 -0400

    Got it technically running. Still not training well.

--- sad_nns/hpo/TD3_hpo.py
+++ sad_nns/hpo/TD3_hpo.py
@@ -52,7 +52,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "RegressionEnv-v0"
     """the id of the environment"""
-    total_timesteps: int = 10_000
+    total_timesteps: int = 50_000
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
@@ -64,11 +64,11 @@ class Args:
     """target smoothing coefficient (default: 0.005)"""
     batch_size: int = 256
     """the batch size of sample from the reply memory"""
-    policy_noise: float = 0.2
+    policy_noise: float = 0.1 #IDK just trying to make it match
     """the scale of policy noise"""
     exploration_noise: float = 0.1
     """the scale of exploration noise"""
-    learning_starts: int = 250#25e3
+    learning_starts: int = 5000 #Matching again #250#25e3
     """timestep to start learning"""
     policy_frequency: int = 2
     """the frequency of training policy (delayed)"""
@@ -83,6 +83,7 @@ def make_env(env_id, seed, idx, capture_video, run_name, env_kwargs: dict = {}):
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         else:
             env = gym.make(env_id, **env_kwargs)
+        env = gym.wrappers.FlattenObservation(env)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env.action_space.seed(seed)
         return env
@@ -95,13 +96,13 @@ class QNetwork(nn.Module):
     def __init__(self, env):
         super().__init__()
         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape), 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 1)
 
     def forward(self, x, a):
         x = torch.cat([x, a], 1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.tanh(self.fc2(x))
         x = self.fc3(x)
         return x
 
@@ -241,6 +242,7 @@ poetry run pip install "stable_baselines3==2.0.0a1"
                 qf2_next_target = qf2_target(data.next_observations, next_state_actions)
                 min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
                 next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (min_qf_next_target).view(-1)
+                # print(next_q_value)
 
             qf1_a_values = qf1(data.observations, data.actions).view(-1)
             qf2_a_values = qf2(data.observations, data.actions).view(-1)
